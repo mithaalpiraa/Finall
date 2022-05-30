be56011699b4afc223cfddca6cea5e82edcf0b5c
mithaalpiraa <102204964+mithaalpiraa@users.noreply.github.com>
2022-05-30 13:35:26 +0800

    Add files via upload

---
+++ MitaAlpira-0128.java
@@ -0,0 +1,13 @@
+package com.mycompany.mitaalpira_0128;
+
+/**
+ *
+ * @author WINDOOWS 10
+ */
+public class RifdatulWafiahPawallungi_0178 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
+
